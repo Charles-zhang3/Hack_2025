23d6c3cc5333538e5c795f452d6d46552b6ec1ff
Charles zhang <zhang_tian01@student.smc.edu>
2025-07-06 21:16:14 -0700

    median

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print("hello_world")
+def median(numbers):
+    numbers_sorted = sorted(numbers)
+    n = len(numbers)
+    if n % 2 == 1:
+        return numbers_sorted[n // 2]
+    else:
+        mid1 = numbers_sorted[n // 2 - 1]
+        mid2 = numbers_sorted[n // 2]
+        return (mid1 + mid2) / 2
+
